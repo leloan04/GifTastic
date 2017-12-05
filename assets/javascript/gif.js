dd1d5c39331f70a38f1ee7426190742f6695e87f
Loan Le <32397554+leloan04@users.noreply.github.com>
2017-12-05 13:53:29 -0500

    updates

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+// $(document).ready(function() {
     //Create a variable to hold array of movie names
     var movies = ["Deadpool", "Bad Moms", "Kingsman", "Underworld", "Iron Man", "The Time Traveler's Wife"];
    
@@ -10,7 +10,7 @@ $(document).ready(function() {
         for (var i = 0; i < movies.length; i++) {
             var a = $("<button>");
             a.addClass("movie");
-            a.attr("data-name", movies[i]);
+            a.attr("data-movie", movies[i]);
             a.text(movies[i]);
             a.on("click", clickMovieButton);
             $("#movieButtons").append(a);
@@ -21,6 +21,7 @@ $(document).ready(function() {
     function clickMovieButton() {
     
         var movie = $(this).attr("data-movie");
+        console.log(movie);
         //Link Gif Url and API key
         var queryURL = "https://api.giphy.com/v1/gifs/search";
 
@@ -36,14 +37,18 @@ $(document).ready(function() {
 
             .done(function (response) {
                 var results = response.data;
+                var gifHolder = $("#displayGifs");
+
+                gifHolder.empty();
 
                 for (var i = 0; i < response.data.length; i++) {
+                    
                     var movieDiv = $("<div>").addClass("item");
                     var rating = results[i].rating;
                     var textRating = $("<p>").text("Rating: " + rating);
                     var movieImage = $("<img>");
 
-                    movieImage.attr("src", results[i].imges.fixed_height.url);
+                    movieImage.attr("src", results[i].images.fixed_height_still.url);
                     movieImage.attr("data-still", results[i].images.fixed_height_still.url);
                     movieImage.attr("data-animate", results[i].images.fixed_height.url);
                     movieImage.attr("data-state", "still");
@@ -63,14 +68,13 @@ $(document).ready(function() {
                     movieDiv.append(textRating);
                     movieDiv.append(movieImage);
 
-                    $("#displayGifs").prepend(movieDiv);
+                    $("#displayGifs").append(movieDiv);
                 }
             })
     }
 
-    clickMovieButton();
    //Create a function that will display x number of Gifs for each movie
    //Create a function that will make the image still and animate when clicked on
    //Display all buttons and Gifs to HTML
 
-});
+// });
